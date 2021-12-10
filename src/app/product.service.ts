7bf363d2ec616acb7557038ca23ec5f195e04025
Nickiefaith <nicolafaith0@gmail.com>
2021-12-10 15:52:38 +0200

    done

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Http} from '@angular/http';
-import { Response} from '@angular/http';
+import { Http,Response} from '@angular/http';
+
+import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs';
 
 
